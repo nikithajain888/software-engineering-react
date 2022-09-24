d033bacec9798b6680de9c81c6d18e2b8b3ca975
nikithajain888 <nikithajain888@github.com>
2022-09-24 13:54:37 -0400

    A1 deliverables

---
+++ build/precache-manifest.89eac8c1b631f786994f15abe87e0e83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ad5d4a9605882c392e8",
+    "url": "/static/css/main.eb95596e.chunk.css"
+  },
+  {
+    "revision": "5ad5d4a9605882c392e8",
+    "url": "/static/js/main.21eea15f.chunk.js"
+  },
+  {
+    "revision": "47a98b626ef86d6828ba",
+    "url": "/static/js/runtime~main.0413ad64.js"
+  },
+  {
+    "revision": "999ac2ca7ad6da8f981d",
+    "url": "/static/js/2.75597be5.chunk.js"
+  },
+  {
+    "revision": "70380ff939c998672254",
+    "url": "/static/js/3.b7dfee96.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "0a37438b68d3ca32a1252ba46925550c",
+    "url": "/index.html"
+  }
+];
